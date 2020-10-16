1b3636fe2de1f459893c1dc624c6bf0676c89982
kavin-du <ukdkcm@gmail.com>
2020-10-16 19:38:16 +0530

    more to do 2

--- TrieDic.c
+++ TrieDic.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// size of the alphabet
 #define CHAR_SET 26
 
 
@@ -11,17 +12,23 @@ struct TrieNode{
 	
 };
 
+// 
 struct TrieNode* createNode(){
+	// allocating memory for new node
 	struct TrieNode* newNode = (struct TrieNode*)malloc(sizeof(struct TrieNode));
+	
+	// making all children null
 	for(int i=0; i<CHAR_SET; i++){
 		newNode->character[i] = NULL;
 	}
+	
+	// intially not a word
 	newNode->isWord = 0;
 	
 	return newNode;
 }
 
-void insertWord(struct TrieNode* head,char* word){
+void insertWord(struct TrieNode* head, char* word){
 	struct TrieNode* traveler = head;
 	while(*word){
 		if(traveler->character[*word -'a'] == NULL){
@@ -39,13 +46,38 @@ void insertWord(struct TrieNode* head,char* word){
 	traveler->isWord = 1;
 }
 
-int printSuggestions(struct TrieNode* head,char* wordPart){
+int searchWord(struct TrieNode* head, char* word){
+	if(head == NULL) return 0;
+
+	struct TrieNode* traverler = head;
+
+	while(*word){
+
+		traverler = traverler->character[*word-'a'];
+
+		if(traverler == NULL) return 0;
+
+		word++;
+	}
+	return traverler->isWord;
+}
+
+
+void printSuggestions(struct TrieNode* head,char* wordPart){
+	
+	if(head == NULL) {
+		printf("Head pointer is null, no words found");
+	}
+	
 	struct TrieNode* traveler = head;
+
+	char* originalWord = wordPart; // because travel changes
+
 	while(*wordPart){
 		if(traveler->character[*wordPart-'a'] == NULL){
-			//printf("No words found!!");
-			return 0;
-			break;
+			printf("No words found!!");
+			return;
+			//break; // no break after return
 		}
 		
 		// move to next node
@@ -54,27 +86,43 @@ int printSuggestions(struct TrieNode* head,char* wordPart){
 		// move to next character
 		wordPart++;
 	}
+
 	int isEmpty = 1;
 	for(int i=0; i<CHAR_SET; i++){
 		if(traveler->character[i] != NULL){
 			isEmpty = 0;
+			break;
 		}
 	}
+	if(isEmpty) printf("%s\n", originalWord);
+	else printNode(traveler);
 	
 }
 
 void printNode(struct TrieNode* node){
-	if(node == NULL){
+	if(node == NULL){ // skip empty nodes
 		return;
 	}
+	printf("%c", node -> data);
+
+	if(node->isWord) printf("\n");
+	
 	for(int i=0; i<CHAR_SET; i++){
-		printf("%c", node -> data);
 		printNode(node -> character[i]);
 	}
 }
 int main(){
 	
+	struct TrieNode* head = createNode();
+
+	char words[][10] = {"hello", "who", "are", "helloww", "you"};
+
+	for(int i=0; i<5; i++){
+		insertWord(head, words[i]);
+	}
+
+	//printf("%s\n", searchWord(head, "hwll") ? "found" : "not found");
 	
-	
-	
+	printNode(head);
+	//printSuggestions(head, "hell");
 }
